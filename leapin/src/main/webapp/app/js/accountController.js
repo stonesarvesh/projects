72e61867b1143374caa36f4ebe55298aab28ae0f
sarvesh <sarvesh@sarvesh-Satellite-C660>
2015-02-08 17:00:49 +0530

    tabs switching

--- leapin/src/main/webapp/app/js/accountController.js
+++ leapin/src/main/webapp/app/js/accountController.js
@@ -1,12 +1,18 @@
-( function ($j) {
+(function($j) {
 	var result = angular.module("app.admin", []);
-	
-	result.controller("AdminController",['$scope','$rootScope','$http', '$filter',function($scope, $rootScope,$http,$filter) {
-		
-		$scope.showMyProfileTab = function () {
-			
-		}; 
-	
-	}]);
+
+	result.controller("AdminController", [ '$scope', '$rootScope', '$http',
+			'$filter', function($scope, $rootScope, $http, $filter) {
+
+				$scope.currentTab = 0;
+
+				$scope.showMyProfileTab = function() {
+					$scope.currentTab = 0;
+				};
+
+				$scope.showNotificationTab = function() {
+					$scope.currentTab = 1;
+				};
+
+			} ]);
 })(jQuery);
-	
